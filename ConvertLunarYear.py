580cdb660c5c20c7a1b2e7126e7d32b6c8499047
tgv293 <102977775+tgv293@users.noreply.github.com>
2024-02-19 16:33:35 +0700

    up

---
+++ ConvertLunarYear.py
@@ -0,0 +1,15 @@
+# #Nhập 1 năm dương lịch, in ra năm âm lịch tương ứng
+# can = ['Canh', 'Tân', 'Nhâm', 'Quý', 'Giáp', 'Ất', 'Bính', 'Đinh', 'Mậu', 'Kỷ']
+# chi = ['Thân', 'Dậu', 'Tuất', 'Hợi', 'Tý', 'Sửu', 'Dần', 'Mão', 'Thìn', 'Tỵ', 'Ngọ', 'Mùi']
+
+# def convert_to_lunar_year(year):
+#     can_index = year % 10
+#     chi_index = year % 12
+#     return can[can_index] + " " + chi[chi_index]
+
+# year = int(input("Nhập năm dương lịch: "))
+# print("Năm âm lịch tương ứng: ", convert_to_lunar_year(year))
+
+st = "Nha Trang Khánh Hòa" * 3
+words = st.split()
+print(words)
